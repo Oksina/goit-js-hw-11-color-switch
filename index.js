182a1b5a12204057e4c2646311ce8760e7d272b2
Oksana Yurchenkovych <oksanayurchenkovych@gmail.com>
2021-01-22 16:08:28 +0200

    зміна кольорів

---
+++ index.js
@@ -0,0 +1,42 @@
+const btn = document.querySelectorAll('button');
+const body = document.body;
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+let bul = false;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+btn[0].addEventListener('click', onChangeColor)
+
+function onChangeColor() {
+    btn[0].removeEventListener('click', onChangeColor);
+    window.colorBody = setInterval(colorOfBody, 1000);
+    btn[1].addEventListener('click', offChangeColor);
+} 
+
+function colorOfBody() {
+  body.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length)];
+}
+
+function offChangeColor() {
+  clearInterval(window.colorBody);
+  changeDisabled();
+  btn[0].addEventListener('click', onChangeColor);
+  btn[1].removeEventListener('click', offChangeColor);
+}
+
+function changeDisabled() {
+  btn[1].disabled = bul;
+  btn[0].disabled = !bul;
+  bul = !bul;
+}
